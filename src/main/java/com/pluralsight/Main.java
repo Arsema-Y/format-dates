8a8026fc2e2c5c7c7cb026d3f5c70ac3a126c4d4
mili Arsema <meleymm88@gmail.com>
2026-04-22 11:22:59 -0500

    imported DateTime and Local time classes

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
+
+import static java.util.Locale.US;
+
 public class Main {
+    public static void main(String[] args) {
+        LocalDateTime today = LocalDateTime.now();
+        TimeZone timeZone = TimeZone.getTimeZone("US/Central");
+
+
+
+
+    }
 }
